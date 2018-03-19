db074fbaee8e2b09312f2f33c8c88b7f1bbc0b9c
kahcheong <kc16rr@brocku.ca>
2018-03-19 13:54:01 -0400

    setup card scripts

---
+++ Paradigm Shuffle/Assets/Scripts/SpawnRoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRoom : MonoBehaviour {
+
+    public playerDeck deck;
+    public GameObject spawnLoc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            LoadRoom(deck.deck[Random.Range(0, deck.deck.Length)], deck.deck[Random.Range(0, deck.deck.Length)]);
+        }
+	}
+
+    void LoadRoom (Card c1, Card c2)
+    {
+
+    }
+}
---
+++ Paradigm Shuffle/Assets/Scripts/playerDeck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDeck : MonoBehaviour {
+
+    public Card[] deck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
