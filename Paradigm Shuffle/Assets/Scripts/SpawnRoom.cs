adf1c112fb979a4976b6067dd639fa1818edc75a
kahcheong <kc16rr@brocku.ca>
2018-03-20 15:28:40 -0400

    deck work now

--- Paradigm Shuffle/Assets/Scripts/SpawnRoom.cs
+++ Paradigm Shuffle/Assets/Scripts/SpawnRoom.cs
@@ -4,24 +4,100 @@ using UnityEngine;
 
 public class SpawnRoom : MonoBehaviour {
 
-    public playerDeck deck;
     public GameObject spawnLoc;
+    public int deckSize;
 
-	// Use this for initialization
-	void Start () {
-		
+    public List<Card> deck = new List<Card>();
+    public List<Card> discard = new List<Card>();
+    public List<Card> availCards = new List<Card>();
+    public List<Card> npcDeck = new List<Card>();
+
+    // Use this for initialization
+    void Start () {
+        deckSize = 4;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.P))
+        
+		if (Input.GetKeyDown(KeyCode.P)) // room load
         {
-            LoadRoom(deck.deck[Random.Range(0, deck.deck.Length)], deck.deck[Random.Range(0, deck.deck.Length)]);
+            int temp1 = 0;
+            int temp2 = 0;
+            if (deckSize >= 2)
+            {
+                temp1 = Random.Range(0, deckSize+1);
+                deckSize--;
+                temp2 = Random.Range(0, deckSize+1);
+                deckSize--;
+            }
+
+            Card temp3 = deck[temp1];
+            deck.RemoveAt(temp1);
+            discard.Add(temp3);
+
+            Card temp4 = deck[temp2];
+            deck.RemoveAt(temp2);
+            discard.Add(temp4);
+
+            LoadRoom(temp3, temp4);
+            
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.P)) // item creation
+        {
+            int temp1 = 0;
+            int temp2 = 0;
+            int temp3 = 0;
+            if (deckSize >= 3)
+            {
+                temp1 = Random.Range(0, deckSize + 1);
+                deckSize--;
+                temp2 = Random.Range(0, deckSize + 1);
+                deckSize--;
+                temp3 = Random.Range(0, deckSize + 1);
+                deckSize--;
+            }
+
+            Card temp4 = deck[temp1];
+            deck.RemoveAt(temp1);
+            discard.Add(temp4);
+
+            Card temp5 = deck[temp2];
+            deck.RemoveAt(temp2);
+            discard.Add(temp5);
+
+            Card temp6 = deck[temp3];
+            deck.RemoveAt(temp3);
+            discard.Add(temp6);
+
+            CreateItem(temp4, temp5,temp6);
+            
+        }
+    }
 
     void LoadRoom (Card c1, Card c2)
     {
+        var card1 = Instantiate(c1, spawnLoc.transform);
 
+        var card2 = Instantiate(c2, spawnLoc.transform);
+
+    }
+
+    void CreateItem(Card c1, Card c2, Card c3)
+    {
+        var card1 = Instantiate(c1, spawnLoc.transform);
+
+        var card2 = Instantiate(c2, spawnLoc.transform);
+
+        var card3 = Instantiate(c2, spawnLoc.transform);
+
+    }
+
+    void ReShuffleDeck()
+    {
+        deck.AddRange(discard);
+        //discard.RemoveAll();
+        deckSize = 20;
     }
 }
