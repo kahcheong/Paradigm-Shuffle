1266a185a2febb2576147bacb48c1304416147dc
kahcheong <kc16rr@brocku.ca>
2018-03-27 21:38:14 -0400

    atk move

--- Paradigm Shuffle/Assets/Scripts/FollowMouse.cs
+++ Paradigm Shuffle/Assets/Scripts/FollowMouse.cs
@@ -4,20 +4,62 @@ using UnityEngine;
 
 public class FollowMouse : MonoBehaviour {
 
-	// Use this for initialization
-
+    // Use this for initialization
+    public bool atk;
+    public int atkType;
+    public GameObject animation;
 
     void Update()
     {
         //rotation
         Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 5.23f;
+        mousePos.z = 0;
 
         Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
         mousePos.x = mousePos.x - objectPos.x;
         mousePos.y = mousePos.y - objectPos.y;
 
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-        transform.localRotation = Quaternion.Euler(new Vector3(0,0, angle));
+        if (!atk)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            animation.GetComponent<Animator>().Play("idle");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            atk = true;
+            StartCoroutine(Atacking());
+            Debug.Log("clicking");
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1)) atkType = 1;
+        if (Input.GetKeyDown(KeyCode.Keypad2)) atkType = 2;
+        if (Input.GetKeyDown(KeyCode.Keypad3)) atkType = 3;
+        if (Input.GetKeyDown(KeyCode.Keypad4)) atkType = 4;
+
+
+
+
     }
+
+    IEnumerator Atacking()
+    {
+        if (atkType == 1)
+        {
+            animation.GetComponent<Animator>().Play("swipe");
+            Debug.Log("attaking swipe");
+            yield return new WaitForSeconds(0.10F);
+        }
+        if (atkType == 2)
+        {
+            animation.GetComponent<Animator>().Play("stab");
+            Debug.Log("attaking stab");
+            yield return new WaitForSeconds(0.09F);
+        }
+        atk = false;
+        yield return true;
+    }
+
+
+    
 }
