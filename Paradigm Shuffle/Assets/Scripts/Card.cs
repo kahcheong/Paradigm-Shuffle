51bdfec5bb71c54c5dd04f08dfbb797766711ee1
melsinaow <melsinaow@gmail.com>
2018-03-31 15:58:44 -0400

    rOOOMS
    Spawn points for rooms and such

--- Paradigm Shuffle/Assets/Scripts/Card.cs
+++ Paradigm Shuffle/Assets/Scripts/Card.cs
@@ -6,13 +6,20 @@ public class Card : MonoBehaviour {
 
     public int id;
     public string Name;
-    public GameObject Enemy;
+    public GameObject enemy;
+    // minions to spawn for special enemies
+    
+    public bool spawnsMinion;
+    public GameObject minion;
+
+    public int minionCount; //how many at a time
+    public int minionFrequency; // how long between spawns in seconds
+    //spaces in room to spawn
     public int minInstance;
     public int maxInstance;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
