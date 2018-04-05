6d4fc6401fab5aaceb8815bfd105f34fb5d858aa
kahcheong <kc16rr@brocku.ca>
2018-04-05 15:59:21 -0400

    hide card back

--- Paradigm Shuffle/Assets/Scripts/UI/library.cs
+++ Paradigm Shuffle/Assets/Scripts/UI/library.cs
@@ -7,17 +7,24 @@ using UnityEngine.EventSystems;
 public class library : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
 
     public GameObject info;
+    public GameObject hide;
 
-        
+    public void Start()
+    {
+        hide = GameObject.Find("CB");
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         info.SetActive(true);
+        hide.SetActive(false);
 
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         info.SetActive(false);
+        hide.SetActive(true);
     }
 
 }
---
+++ Paradigm Shuffle/Assets/Scripts/enemy/slime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slime : MonoBehaviour
+{
+
+    public float speed;
+    public float damage;
+    public Enemy awake;
+
+
+
+    private void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player" && tag == "weapon")
+        {
+            float temp = damage - other.GetComponent<Player>().damageReducFlat;
+            if (temp > 0) other.GetComponent<Player>().hp -= temp * (1f - other.GetComponent<Player>().damageReducPercent);
+        }
+        if (other.tag == "wall")
+        {
+            
+        }
+
+    }
+
+}
