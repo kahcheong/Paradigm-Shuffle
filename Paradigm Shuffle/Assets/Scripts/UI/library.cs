c32d673b00971b9123085809df2353d231a60c1e
kahcheong <kc16rr@brocku.ca>
2018-04-05 15:46:27 -0400

    library

---
+++ Paradigm Shuffle/Assets/Scripts/UI/library.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class library : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+
+    public GameObject info;
+
+        
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        info.SetActive(true);
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        info.SetActive(false);
+    }
+
+}
