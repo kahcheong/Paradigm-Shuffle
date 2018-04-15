be7c07f6dd93f9cb0a87a29a4dcbd6f77615b46c
kahcheong <kc16rr@brocku.ca>
2018-04-15 03:19:23 -0400

    oof

---
+++ Paradigm Shuffle/Assets/Scripts/FloorManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour {
+
+    public static FloorManager floorManager;
+
+    public List<room> rooms = new List<room>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
