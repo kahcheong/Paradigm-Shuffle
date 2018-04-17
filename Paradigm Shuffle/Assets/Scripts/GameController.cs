58630f869b839dc9d95adafda5ef9ad98cd16ca5
kahcheong <kc16rr@brocku.ca>
2018-04-16 23:08:46 -0400

    oof

--- Paradigm Shuffle/Assets/Scripts/GameController.cs
+++ Paradigm Shuffle/Assets/Scripts/GameController.cs
@@ -126,16 +126,5 @@ public class GameController : MonoBehaviour {
 
     
 
-    public void Shuffle<T>(this IList<T> list)
-    {
-        int n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = rng.Next(n + 1);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
-        }
-    }
+
 }
