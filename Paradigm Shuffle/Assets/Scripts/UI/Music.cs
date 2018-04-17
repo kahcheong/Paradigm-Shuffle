6817fad60e1c8fd7b0112a5d87eaf3a621f6cd30
melsinaow <melsinaow@gmail.com>
2018-04-16 22:03:21 -0400

    sound

---
+++ Paradigm Shuffle/Assets/Scripts/UI/Music.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour
+{
+
+    public AudioSource audio;
+    public AudioClip[] myMusic = new AudioClip[3];
+    // Use this for initialization
+    void Awake()
+    {
+                audio.clip = myMusic[0] as AudioClip;
+    }
+    void Start()
+    {
+
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audio.isPlaying)
+            playRandomMusic();
+    }
+    void playRandomMusic()
+    {
+        audio.clip = myMusic[Random.Range(0, myMusic.Length)] as AudioClip;
+        audio.Play();
+    }
+}
